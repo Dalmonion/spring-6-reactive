b5792a000ade000761306ae5e2db9a496f0795b6
dgrudzinski <dgrudzinski@dgrudzinskis-MacBook-Pro.local>
2024-08-20 12:02:49 +0200

    Spring security config

--- src/main/java/com/example/spring6reactive/config/SecurityConfig.java
+++ src/main/java/com/example/spring6reactive/config/SecurityConfig.java
@@ -1,10 +1,14 @@
 package com.example.spring6reactive.config;
 
+import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
@@ -12,6 +16,17 @@ import org.springframework.security.web.server.SecurityWebFilterChain;
 public class SecurityConfig {
 
     @Bean
+    @Order(1)
+    public SecurityFilterChain actuatorSecurityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .authorizeHttpRequests(authorizationManagerRequestMatcherRegistry
+                                               -> authorizationManagerRequestMatcherRegistry.requestMatchers(
+                        EndpointRequest.toAnyEndpoint()).permitAll());
+        return httpSecurity.build();
+    }
+
+    @Bean
+    @Order(2)
     SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity security) {
         security.csrf(ServerHttpSecurity.CsrfSpec::disable)
                 .authorizeExchange(authorizeExchangeSpec -> authorizeExchangeSpec.anyExchange().authenticated())
